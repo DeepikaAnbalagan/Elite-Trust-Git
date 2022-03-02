eaf253c4f921dcdff17d4f8033e1bf3ecf8bf5ce
DeepikaAnbalagan <92775307+DeepikaAnbalagan@users.noreply.github.com>
2022-03-02 12:54:21 +0530

    new changes

--- Elite.php
+++ Elite.php
@@ -435,7 +435,7 @@
                         </div>
                         </div>
                         <div class="submit">
-                           <button type="submit" name="submit"  class="btn btn-primary">submit2</button>
+                           <button type="submit" name="submit"  class="btn btn-primary">submited</button>
                         </div>
                     </div>
                 </div>
